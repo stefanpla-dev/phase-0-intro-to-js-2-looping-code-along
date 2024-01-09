4fd713d125796b07f721574c2d541336528fa3d9
Stefan Pla <spla@icloud.com>
2024-01-09 11:50:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const besties = ["Nick", "Drew", "Kate"];
+
+function writeCards(besties){
+    const thankYouMessages = [];
+
+    for (let i=0; i <besties.length; i++){
+        const thankYouMessage = `Thank you, ${besties[i]}, for the wonderful surprise gift!`;
+        thankYouMessages.push(thankYouMessage);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number){
+    let i = number;
+    while (i>=0) {
+        console.log(i);
+        i--;
+    }
+}
